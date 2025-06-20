ab243cef4363e83adc19e6870b8364117b6555fe
Piyushkr001 <piyushthegreat2305@gmail.com>
2025-06-20 23:40:22 +0530

    `Update Dashboard page with additional information and layout changes`

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -3,8 +3,11 @@ import Layout from '../components/Layout';
 export default function Dashboard() {
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">Welcome to Excel Analytics Platform</h1>
-      <p className="text-gray-600 mt-2">You're now logged in 🎉</p>
+      <div className='p-5 flex flex-col items-center justify-center'>
+        <h1 className="text-2xl font-bold">Welcome to Excel Analytics Platform</h1>
+        <p className="text-gray-600 mt-2"> You're now logged in 🎉 Use the sidebar to upload Excel files, view history, or access admin tools.
+        </p>
+      </div>
     </Layout>
   );
 }
