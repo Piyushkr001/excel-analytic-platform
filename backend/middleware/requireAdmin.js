7a78cc8fe4b3ab66c41bb704a94e0480b9906baa
rajshree29 <rajshreetembhare29@gmail.com>
2025-07-07 13:18:48 +0530

    updated files

--- backend/middleware/requireAdmin.js
+++
@@ -1,6 +0,0 @@
-export const requireAdmin = (req, _res, next) => {
-  if (req.user?.role !== "admin") {
-    return next({ status: 403, message: "Admin only" });
-  }
-  next();
-};
--- backend/models/ExcelRecord.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-const excelRecordSchema = new mongoose.Schema({}, { strict: false });
-
-const ExcelRecord = mongoose.model('ExcelRecord', excelRecordSchema);
-export default ExcelRecord;
